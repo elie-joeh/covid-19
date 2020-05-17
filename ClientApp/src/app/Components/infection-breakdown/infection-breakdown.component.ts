068519849186c9fcc9df873e82793fc147709862
Adam Galati <59342033+adamgalaxi@users.noreply.github.com>
2020-05-17 11:36:29 -0400

    UI + BackEnd integrate (#3)
    * C# .net backend setup with current UI integrated
    
    * C# .net backend setup with current UI integrated
    
    * back integration

---
+++ ClientApp/src/app/Components/infection-breakdown/infection-breakdown.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, Input, ErrorHandler } from '@angular/core';
+import { InfectionBreakdownService } from '../../Services/infection-breakdown.service';
+import { Infection_info } from '../../Interfaces/infection-info';
+
+@Component({
+  selector: 'app-infection-breakdown',
+  templateUrl: './infection-breakdown.component.html',
+  styleUrls: ['./infection-breakdown.component.sass']
+})
+export class InfectionBreakdownComponent implements OnInit {
+
+
+  constructor(private infectionBreakdownService : InfectionBreakdownService) { }
+
+  selected_province: string = 'Quebec';
+
+  all_infection_info : Infection_info[];
+
+  ngOnInit(): void {
+    this.infectionBreakdownService.selected_province_obs$.subscribe(
+      data => this.handleNewProvince(data)
+    );
+    this.getInfectionBreakdown()
+  }
+
+  getInfectionBreakdown() {
+    this.infectionBreakdownService.getInfectionInfo()
+      .subscribe(infection_info => this.all_infection_info = infection_info)
+    
+      //TODO: sort the provinces by infection
+  }
+
+  getInfectionByProvince(selected_province: string){
+    console.log(`getting infection information by province ${selected_province}`)
+    //TODO subscribe to the observable that you are receiving
+    //this.infectionBreakdownService.getInfecionInfoByProvince(selected_province)
+    
+  }
+
+
+  //TODO: check if the data is incorrect and handle it
+  handleNewProvince(data) {
+    this.selected_province = data;
+    this.getInfectionBreakdown();
+  }
+
+}
