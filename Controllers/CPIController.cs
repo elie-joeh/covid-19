e09085263a3aa2cf39152d7258abae2b0fa72baa
elie-joeh <elie-joe.haikal@mail.mcgill.ca>
2020-06-17 12:33:58 -0400

    Created SQL database, populated it with CPI data

---
+++ Controllers/CPIController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using covid19.Data;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace covid19.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CPIController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private ICPIService _service;
+        public CPIController(ApplicationDbContext context, ICPIService service)
+        {
+            _context = context;
+            _service = service;
+        }
+
+        //Get: api/getCPIs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CPI>>> getCPIs()
+        {
+            return await _context.CPIs;
+        }
+
+        
+        // GET: api/Cities/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<City>> GetCPIByDGUID(string DGUID)
+        {
+            var CPIs = await _context.CPIs.;
+            if (city == null)
+            {
+            return NotFound();
+            }
+            return city;
+        }
+
+
+    }
+}
